ba3f17982a5591216b7c5b58d01cadf0bf21d4cf
AmbikaSrikanth <sree.ambi3117@gmail.com>
2019-07-16 19:10:50 +0530

    added to local repo

---
+++ src/com/hb/test/GeneratorTest.java
@@ -0,0 +1,45 @@
+package com.hb.test;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.hb.entity.InsurancePolicy;
+
+import com.hb.utility.HibernateUtil;
+
+public class GeneratorTest {
+	public static void main(String[] args) {
+		Session ses=null;
+		InsurancePolicy policy=null;
+		Transaction tx=null;
+		boolean flag=false;
+		int idval=0;
+		ses=HibernateUtil.getSession();
+		policy=new InsurancePolicy();
+		policy.setPolicyId(200);
+		policy.setPolicyName("saaadhar");
+		policy.setClosingTime(23);
+		policy.setTenture(24);
+		try{
+			tx=ses.beginTransaction();
+			idval=(int) ses.save(policy);
+			//ses.flush();
+			flag=true;
+		}catch(Exception e){
+			e.printStackTrace();
+			flag=false;
+		}
+		finally{
+			
+			if(flag){
+				tx.commit();
+			System.out.println("object is saved"+idval);
+		}else{
+			tx.rollback();
+			System.out.println("object is not saved");
+		}
+		HibernateUtil.closeSession(ses);
+		HibernateUtil.closeSessionFactory();
+	}
+	}
+}
